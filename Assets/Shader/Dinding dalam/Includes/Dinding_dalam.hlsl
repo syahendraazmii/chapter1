39d1bd8a0908442ae11077935640d84e2caad2b2
ucup Markucup <81012410+Yusuf19-rgb@users.noreply.github.com>
2024-05-13 17:54:07 +0700

    update selessai
    karek ngatur lightning

--- Assets/Shader/Dinding dalam/Includes/Dinding_dalam.hlsl
+++ Assets/Shader/Dinding dalam/Includes/Dinding_dalam.hlsl
@@ -1,20 +1,20 @@
 #include <Packages/com.blendernodesgraph.core/Editor/Includes/Importers.hlsl>
 
-void Dinding_dalam_float(float3 _POS, float3 _PVS, float3 _PWS, float3 _NOS, float3 _NVS, float3 _NWS, float3 _NTS, float3 _TWS, float3 _BTWS, float3 _UV, float3 _SP, float3 _VVS, float3 _VWS, float2 name, Texture2D image_38854, Texture2D image_38852, Texture2D image_38856, Texture2D image_38864, out float4 Color, out float3 Normal, out float Smoothness, out float4 Emission, out float AmbientOcculusion, out float Metallic, out float4 Specular)
+void Dinding_dalam_float(float3 _POS, float3 _PVS, float3 _PWS, float3 _NOS, float3 _NVS, float3 _NWS, float3 _NTS, float3 _TWS, float3 _BTWS, float3 _UV, float3 _SP, float3 _VVS, float3 _VWS, float2 name, Texture2D image_13922, Texture2D image_13924, Texture2D image_13926, Texture2D image_13930, out float4 Color, out float3 Normal, out float Smoothness, out float4 Emission, out float AmbientOcculusion, out float Metallic, out float4 Specular)
 {
 	
-	float4 _ImageTexture_38856 = node_image_texture(image_38856, _UV, 0);
-	float4 _ImageTexture_38864 = node_image_texture(image_38864, name, 0);
-	float4 _Invert_38862 = node_invert(1, _ImageTexture_38864);
-	float4 _NormalMap_38850; node_normal_map(_Invert_38862, 1, _NormalMap_38850);
-	float4 _ImageTexture_38854 = node_image_texture(image_38854, _UV, 0);
-	float4 _ImageTexture_38852 = node_image_texture(image_38852, _UV, 0);
+	float4 _ImageTexture_13926 = node_image_texture(image_13926, name, 0);
+	float4 _ImageTexture_13930 = node_image_texture(image_13930, name, 0);
+	float4 _Invert_13932 = node_invert(1, _ImageTexture_13930);
+	float4 _NormalMap_13920; node_normal_map(_Invert_13932, 1, _NormalMap_13920);
+	float4 _ImageTexture_13922 = node_image_texture(image_13922, name, 0);
+	float4 _ImageTexture_13924 = node_image_texture(image_13924, name, 0);
 
-	Color = _ImageTexture_38856;
-	Normal = _NormalMap_38850;
+	Color = _ImageTexture_13926;
+	Normal = _NormalMap_13920;
 	Smoothness = 0.0;
-	Emission = _ImageTexture_38854;
+	Emission = _ImageTexture_13922;
 	AmbientOcculusion = 0.0;
-	Metallic = _ImageTexture_38852;
+	Metallic = _ImageTexture_13924;
 	Specular = float4(0.0, 0.0, 0.0, 0.0);
 }
